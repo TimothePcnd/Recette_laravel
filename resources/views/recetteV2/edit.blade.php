262a6eef391a14e97c22b95e90630a490f8bd12b
timothe.pecnard <timothe.pecnard@campus-centre.fr>
2025-05-22 16:05:29 +0200

    design bootstrap

--- resources/views/recetteV2/edit.blade.php
+++ resources/views/recetteV2/edit.blade.php
@@ -3,18 +3,21 @@
 
 @section('content')--}}
     <h1>Edit Post</h1>
-    <form method="POST" action="{{ route('recetteV2.update', $recetteV2->id) }}" enctype="multipart/form-data">
+
+    <form method="POST" action="{{ route('recetteV2.update', $recetteV2->id) }}" enctype="multipart/form-data" class="p-4">
         @csrf
         @method('PUT')
-        <div class="form-group">
-            <label for="title">Title</label>
-            <input type="text" name="title" class="form-control" value="{{ $recetteV2->title }}" required>
+        <div class="mb-3">
+            <label for="title" class="form-label">Titre</label>
+            <input type="text" name="title" id="title" class="form-control" value="{{ $recetteV2->title }}" required>
         </div>
-        <div class="form-group">
-            <label for="description">Description</label>
-            <textarea name="description" class="form-control" required>{{ $recetteV2->description }}</textarea>
+
+        <div class="mb-3">
+            <label for="description" class="form-label">Description</label>
+            <textarea name="description" id="description" class="form-control" required>{{ $recetteV2->description }}</textarea>
         </div>
-        <button type="submit" class="btn btn-primary mt-3">Update</button>
+
+        <button type="submit" class="btn btn-primary">Mettre à jour</button>
     </form>
 {{--
 @endsection --}}
